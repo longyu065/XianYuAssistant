b66a3ee2d3631cc9a408c016a820edcee0b771bf
IAMLZY <iamlzy2020@163.com>
2025-11-11 21:48:51 +0800

    初始化代码仓库以及开发规范

---
+++ src/main/java/com/feijimiao/xianyuassistant/XianYuAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.feijimiao.xianyuassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XianYuAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XianYuAssistantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/feijimiao/xianyuassistant/XianYuAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feijimiao.xianyuassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XianYuAssistantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
